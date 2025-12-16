387871d02650eab7a153592cdc592c41e8da7446
haris818 <harismemon422@gmail.com>
2025-12-16 03:01:26 -0800

    express-ejs-web

--- Express Ejs/public/js/index.js
+++
@@ -1,9 +0,0 @@
-console.log("JS connected successfully!");
-
-document.addEventListener("DOMContentLoaded", () => {
-    const img = document.querySelector(".my-image");
-
-    img.addEventListener("click", () => {
-        alert("Image clicked!");
-    });
-});
